2b82f00950a58e16beeb259fde43681e8c226619
Nick Hellemans <NickHellemans93@gmail.com>
2022-08-16 12:10:59 +0200

    style(): better scaling for logos

---
+++ src/molecules/navbar/navbar.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import React from 'react'
+import { Navbar as NavbarComponent, type NavbarProps } from './Navbar'
+
+export default {
+  title: 'Molecules/Navbar',
+  component: NavbarComponent,
+  argTypes: {
+    logo: {
+      control: 'text',
+      defaultValue: undefined,
+    },
+    companyName: {
+      control: 'text',
+      defaultValue: 'Awell Health',
+    },
+  },
+} as Meta
+
+export const Navbar: Story = ({ logo }: NavbarProps) => {
+  return <NavbarComponent logo={logo} />
+}
