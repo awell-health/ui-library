7e97de01cdebf1eca47c685e3fcc6754d440679e
Rahul Keerthi <rahulkeerthi2@gmail.com>
2023-04-06 15:08:52 +0100

    docs(date-picker): add deprecation warning and replace story with input field

--- src/atoms/datePicker/datePicker.stories.tsx
+++ src/atoms/datePicker/datePicker.stories.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Meta, Story } from '@storybook/react/types-6-0'
-import { DatePicker as DatePickerComponent } from '.'
-import { DatePickerProps } from './DatePicker'
 import { ThemeProvider } from '../themeProvider'
+import { InputField, InputFieldProps } from '../inputField'
 
 export default {
-  title: 'atoms/Date Picker',
-  component: DatePickerComponent,
+  title: 'atoms/Input Field',
+  component: InputField,
   argTypes: {
     label: {
       control: 'text',
@@ -17,8 +16,8 @@ export default {
       defaultValue: 'date-picker-story-id',
     },
     value: {
-      control: 'date',
-      defaultValue: new Date(),
+      control: 'string',
+      defaultValue: '2023-03-20',
     },
     mandatory: {
       control: 'boolean',
@@ -41,20 +40,52 @@ export default {
   ],
 } as Meta
 
-export const DatePicker: Story<DatePickerProps> = ({
+export const DateInput: Story<InputFieldProps> = ({
   id,
   label,
   onChange,
   value,
   mandatory,
 }) => {
+  const [date, setDate] = React.useState(value)
+
   return (
-    <DatePickerComponent
-      label={label}
-      mandatory={mandatory}
-      id={id}
-      onChange={onChange}
-      value={value}
-    />
+    <>
+      <InputField
+        type="date"
+        label={label}
+        onChange={(e) => {
+          setDate(e.target.value)
+          onChange(e)
+        }}
+        id={id}
+        value={date}
+        mandatory={mandatory}
+      />
+      <br />
+      <p style={{ width: '40%' }}>
+        If working with incoming values that are Date objects in your app, you
+        can use a <code>formatDate</code> function like below to convert it to a
+        string that can be used in the <code>value</code> prop. The Awell API
+        expects dates to be formatted as <code>yyyy-MM-dd</code>, same as it is
+        for native <code>input</code> elements.
+      </p>
+      <code>
+        <pre>
+          {`
+import { format } from 'date-fns'
+
+const formatDate = (date: Date | null) => {
+  if (!date) return format(new Date(), 'yyyy-MM-dd')
+  return format(date, 'yyyy-MM-dd')
+}
+
+console.log(
+  formatDate(new Date(value)) // => '2023-03-20'
+)
+          `}
+        </pre>
+      </code>
+    </>
   )
 }
