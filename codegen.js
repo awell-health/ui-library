9f2860b7ebb0469c4552684a7a842488d90faa84
Nick Hellemans <nickhellemans@Nicks-MacBook-Pro-2.local>
2023-08-08 18:20:44 +0200

    chore(): add env vars to action config

--- codegen.js
+++ codegen.js
@@ -1,8 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config()
 
-console.log(process.env.CODEGEN_AWELL_API_KEY)
-
 module.exports = {
   overwrite: true,
   schema: {
