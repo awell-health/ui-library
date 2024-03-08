4ba989518278722b328930735a52a19c8088280b
Nick Hellemans <nick@awellhealth.com>
2024-03-08 22:21:13 +0100

    TP-1193: fix traditional form mode layout (#132)
    * fix(css): traditional form mode layout
    
    ---------
    
    Co-authored-by: Nick Hellemans <nickhellemans@Nicks-MacBook-Pro-2.local>

--- src/hostedPages/layouts/HostedPageLayout/HostedPageLayout.tsx
+++ src/hostedPages/layouts/HostedPageLayout/HostedPageLayout.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import { Navbar } from '../../../molecules'
 import { CloseButton } from './CloseButton'
 import classes from './hostedPageLayout.module.scss'
+import { useTheme } from '../../../atoms/themeProvider/ThemeProvider'
 
 export interface HostedPageLayoutProps {
   children: React.ReactNode | string
@@ -16,8 +17,10 @@ export const HostedPageLayout: FC<HostedPageLayoutProps> = ({
   logo,
   hideCloseButton = false,
 }) => {
+  const { layoutMode } = useTheme()
+
   return (
-    <div className={classes.layout_container}>
+    <div className={`${classes.layout_container} ${classes[layoutMode]}`}>
       {!hideCloseButton && <CloseButton onClose={onCloseHostedPage} />}
       <header>
         <Navbar logo={logo} />
