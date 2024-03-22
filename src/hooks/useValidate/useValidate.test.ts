aaee636d5cb7726d80564b0065abc61d1e149916
mohsinht <mohsinhayat104@gmail.com>
2024-03-22 16:14:34 +0500

    feat: add tests for date validation in useValidate hook

--- src/hooks/useValidate/useValidate.test.ts
+++ src/hooks/useValidate/useValidate.test.ts
@@ -1,5 +1,6 @@
 import { renderHook } from '@testing-library/react-hooks'
 import { useValidate } from './useValidate'
+import { AllowedDatesOptions } from '../../types'
 
 describe('useValidate', () => {
   describe('validatePhoneNumber', () => {
@@ -115,6 +116,179 @@ describe('useValidate', () => {
           'ca',
         ])
       expect(matchesAvailableCountries).toBe(false)
+    })
+  })
+
+  describe('validateDateResponse', () => {
+    // Mock current date to ensure consistent results
+    beforeAll(() => {
+      jest.useFakeTimers().setSystemTime(new Date('2024-01-01'))
+    })
+
+    describe('when date configuration is not defined', () => {
+      it('should return isValid set to true', () => {
+        const { result } = renderHook(() => useValidate())
+        const pastDate = '2023-01-01'
+        expect(
+          result.current.validateDateResponse(undefined, pastDate).isValid
+        ).toBe(true)
+        const todayDate = '2024-01-01'
+        expect(
+          result.current.validateDateResponse(undefined, todayDate).isValid
+        ).toBe(true)
+        const futureDate = '2024-01-05'
+        expect(
+          result.current.validateDateResponse(undefined, futureDate).isValid
+        ).toBe(true)
+      })
+    })
+
+    describe('when date should be in the past', () => {
+      it('should return isValid set to true when value is in the past', () => {
+        const dateConfig = {
+          mandatory: false,
+          date: {
+            allowed_dates: AllowedDatesOptions.Past,
+          },
+        }
+        const { result } = renderHook(() => useValidate())
+        const pastDate = '2023-01-01'
+        expect(
+          result.current.validateDateResponse(dateConfig, pastDate).isValid
+        ).toBe(true)
+      })
+
+      it('should return isValid set to false when value is in the future', () => {
+        const dateConfig = {
+          mandatory: false,
+          date: {
+            allowed_dates: AllowedDatesOptions.Past,
+          },
+        }
+        const { result } = renderHook(() => useValidate())
+        const futureDate = '2024-01-02'
+        expect(
+          result.current.validateDateResponse(dateConfig, futureDate).isValid
+        ).toBe(false)
+        expect(
+          result.current.validateDateResponse(dateConfig, futureDate).errorType
+        ).toBe('DATE_CANNOT_BE_IN_THE_FUTURE')
+      })
+
+      it('should return isValid set to false when value is today date and date of response is not allowed', () => {
+        const dateConfig = {
+          mandatory: false,
+          date: {
+            allowed_dates: AllowedDatesOptions.Past,
+          },
+        }
+        const { result } = renderHook(() => useValidate())
+        const todayDate = '2024-01-01'
+        expect(
+          result.current.validateDateResponse(dateConfig, todayDate).isValid
+        ).toBe(false)
+        expect(
+          result.current.validateDateResponse(dateConfig, todayDate).errorType
+        ).toBe('DATE_CANNOT_BE_TODAY')
+      })
+
+      it('should return isValid set to true when value is today date and date of response is allowed', () => {
+        const dateConfig = {
+          mandatory: false,
+          date: {
+            allowed_dates: AllowedDatesOptions.Past,
+            include_date_of_response: true,
+          },
+        }
+        const { result } = renderHook(() => useValidate())
+        const todayDate = '2024-01-01'
+        expect(
+          result.current.validateDateResponse(dateConfig, todayDate).isValid
+        ).toBe(true)
+      })
+    })
+
+    describe('when date should be in the future', () => {
+      it('should return isValid set to true when value is in the future', () => {
+        const dateConfig = {
+          mandatory: false,
+          date: {
+            allowed_dates: AllowedDatesOptions.Future,
+          },
+        }
+        const { result } = renderHook(() => useValidate())
+        const futureDate = '2025-01-01'
+        expect(
+          result.current.validateDateResponse(dateConfig, futureDate).isValid
+        ).toBe(true)
+      })
+
+      it('should return isValid set to false when value is in the future', () => {
+        const dateConfig = {
+          mandatory: false,
+          date: {
+            allowed_dates: AllowedDatesOptions.Future,
+          },
+        }
+        const { result } = renderHook(() => useValidate())
+        const pastDate = '2023-12-02'
+        expect(
+          result.current.validateDateResponse(dateConfig, pastDate).isValid
+        ).toBe(false)
+        expect(
+          result.current.validateDateResponse(dateConfig, pastDate).errorType
+        ).toBe('DATE_CANNOT_BE_IN_THE_PAST')
+      })
+
+      it('should return isValid set to false when value is today date and date of response is not allowed', () => {
+        const dateConfig = {
+          mandatory: false,
+          date: {
+            allowed_dates: AllowedDatesOptions.Future,
+          },
+        }
+        const { result } = renderHook(() => useValidate())
+        const todayDate = '2024-01-01'
+        expect(
+          result.current.validateDateResponse(dateConfig, todayDate).isValid
+        ).toBe(false)
+        expect(
+          result.current.validateDateResponse(dateConfig, todayDate).errorType
+        ).toBe('DATE_CANNOT_BE_TODAY')
+      })
+
+      it('should return isValid set to true when value is today date and date of response is allowed', () => {
+        const dateConfig = {
+          mandatory: false,
+          date: {
+            allowed_dates: AllowedDatesOptions.Future,
+            include_date_of_response: true,
+          },
+        }
+        const { result } = renderHook(() => useValidate())
+        const pastDate = '2024-01-01'
+        expect(
+          result.current.validateDateResponse(dateConfig, pastDate).isValid
+        ).toBe(true)
+      })
+    })
+
+    describe('when all dates are allowed', () => {
+      it('should return isValid set to true', () => {
+        const { result } = renderHook(() => useValidate())
+        const pastDate = '2023-01-01'
+        expect(
+          result.current.validateDateResponse(undefined, pastDate).isValid
+        ).toBe(true)
+        const todayDate = '2024-01-01'
+        expect(
+          result.current.validateDateResponse(undefined, todayDate).isValid
+        ).toBe(true)
+        const futureDate = '2024-01-05'
+        expect(
+          result.current.validateDateResponse(undefined, futureDate).isValid
+        ).toBe(true)
+      })
     })
   })
 })
