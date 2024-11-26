7f767c9f76e7dc895aa257b9aa4f1da51c54a0cf
Nick Hellemans <nick@awellhealth.com>
2024-11-26 17:32:48 +0100

    fix(): escaped strings line breaks

--- src/atoms/richTextViewer/RichTextViewer.tsx
+++ src/atoms/richTextViewer/RichTextViewer.tsx
@@ -8,10 +8,23 @@ interface RichTextViewerProps {
 export const RichTextViewer = ({
   content,
 }: RichTextViewerProps): JSX.Element => {
+  /**
+   * Ensure that escaped new line characters (\n)
+   * are properly rendered as line breaks (<br />) in HTML.
+   *
+   * Example:
+   * Input: "Line 1\nLine 2"
+   * Output: "Line 1<br />Line 2"
+   *
+   * This is particularly useful when dealing with escaped strings
+   * where new lines are indicated as \n and need to be displayed correctly in HTML.
+   */
+  const contentWithLineBreaks = content.split('\n').join('<br />')
+
   return (
     <div
       className={classes.content}
-      dangerouslySetInnerHTML={{ __html: content }}
+      dangerouslySetInnerHTML={{ __html: contentWithLineBreaks }}
     />
   )
 }
